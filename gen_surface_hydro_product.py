484dc1862b1076044afca4ed4e4f13a7622da606
Kyle Taylor <kyle.a.taylor@gmail.com>
2018-03-05 17:30:20 -0700

    house keeping

--- gen_surface_hydro_product.py
+++ gen_surface_hydro_product.py
@@ -1,38 +1,66 @@
-""" This script pulls from the Landsat 8 satellite to generate an estimate of recent surface wetness """
+""" This script pulls from the Landsat 8 satellite to generate an estimate of 
+recent surface wetness """
 
+import config
 import ee
 
-def unixmillisecs_frommonths(pmonths):
-  ''' mucking about with dates and times, the date range is the last 4 months'''
-  return pmonths*1000*60*60*24*30
+def now_minus_n_months(*args):
+  """ accept a single positional argument (month) specifying how far from now
+  to go back in our landsat imagery"""
+  try:
+    now = ee.datetime.datetime.now()
+    dst_year  = now.year
+    dst_month = now.month - args[0]
+    dst_day   = now.day
+    if dst_month < 0:
+      dst_year = dst_year - 1
+      dst_month = dst_month + 12 
+    return(ee.datetime.datetime(dst_year, dst_month, dst_day))  
+  except Exception as e:
+    raise e
 
-
-def imagefromcollection(pcollection):
+def imagefromcollection(pcollection, eethen, eenow):
   '''The image collection and sorting that all out. Get the most recent image from a given collection'''
-  collection = ee.ImageCollection(pcollection).filterDate(eethen, eenow).filterMetadata('CLOUD_COVER', 'less_than', .3);
-  recent = collection.sort('system:time_start', false).limit(1);
-  print(recent);
+  collection = ee.ImageCollection(pcollection).filterDate(eethen).filterMetadata('CLOUD_COVER', 'less_than', .3);
+  recent = collection.sort('system:time_start', False).limit(1);
   return collection.mosaic();
 
 def water(plandsatimage):
-  //extract band4 from image and assign that to variable b4
-  var b4 = plandsatimage.select('B4');
-  //extract band6 from image and assign that to variable b6
-  var b6 = plandsatimage.select('B6');
-  var blank = ee.Image(0);
-  var output = blank.where(b6.lte(b4),1);
+  # extract band4 from image and assign that to variable b4
+  b4 = plandsatimage.select('B4');
+  # extract band6 from image and assign that to variable b6
+  b6 = plandsatimage.select('B6');
+  blank = ee.Image(0);
+  output = blank.where(b6.lte(b4),1);
   return output.updateMask(output);
 
+def exportImageToAsset(image, assetId, region):
+    task_config = {
+      'description': 'dynamic water over the last 10 months',
+      'scale': 30,  
+      'region': region,
+      'maxPixels' : 400000000,
+      'assetId' : assetId
+    }
+    task = ee.batch.Export.image(image, 'LC8dynamicwater', task_config)
+    task.start()
+      
 # MAIN
+
+# Use our App Engine service account's credentials.
+EE_CREDENTIALS = ee.ServiceAccountCredentials(
+    config.EE_ACCOUNT, config.EE_PRIVATE_KEY_FILE)
+    
+ee.Initialize(EE_CREDENTIALS)
 
 # define our time-series information
   
-eenow = ee.Date(Date.now());
-eethen = ee.Date(Date.now() - unixmillisecs_frommonths(10));
+eenow = ee.datetime.datetime.now();
+eethen = now_minus_n_months(10);
 
 # import the geometry for Kansas
-kansas = ee.FeatureCollection('ft:1fRY18cjsHzDgGiJiS2nnpUU3v9JPDc2HNaR7Xk8').
-  filter(ee.Filter.eq('Name', 'Kansas'));
+kansas = ee.FeatureCollection('ft:1fRY18cjsHzDgGiJiS2nnpUU3v9JPDc2HNaR7Xk8').filter(ee.Filter.eq('Name', 'Kansas'));
+
 
 # copy over our water surface as an asset, caching the previous version so that 
 # users that are currently on the website will be able to view the last copy
@@ -41,19 +69,22 @@ try:
   ee.data.deleteAsset('users/adaniels/shared/LC8dynamicwater_cached');
 except Exception as e:
   pass
+
+value = water(imagefromcollection('LANDSAT/LC08/C01/T1', eethen, eenow)).clipToCollection(kansas)
   
-ee.data.copyAsset('users/adaniels/shared/LC8dynamicwater','users/adaniels/shared/LC8dynamicwater_cached');
-ee.data.deleteAsset('users/adaniels/shared/LC8dynamicwater');
+ee.data.copyAsset('users/adaniels/shared/LC8dynamicwater','users/kyletaylor/shared/LC8dynamicwater_cached');
+ee.data.deleteAsset('users/kyletaylor/shared/LC8dynamicwater');
 ee.data.createAsset({
-  image: water(imagefromcollection('LANDSAT/LC08/C01/T1')).clipToCollection(kansas),
-  description: 'dynamicwater_last4months',
-  assetId: 'LC8dynamicwater',
-  scale: 30,
-  region: kansas,
-  pyramidingPolicy: {'.default': 'sample'},
-  maxPixels: 400000000,
-  });
-  
+  'image': water(imagefromcollection('LANDSAT/LC08/C01/T1', eethen, eenow)).clipToCollection(kansas),
+  'description': 'dynamic water over the last 10 months',
+  'assetId': 'users/kyletaylor/shared/LC8dynamicwater',
+  'scale': 30,
+  'region': kansas,
+  'maxPixels': 400000000
+  })
+
+ee.data.createAsset(value, 'users/kyletaylor/shared/LC8dynamicwater')
+
 try: 
   ee.data.deleteAsset('users/adaniels/shared/LC8dynamicwater_cached');
 except Exception as e:

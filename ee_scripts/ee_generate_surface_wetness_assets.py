20d3035dfcf4bd6b4bd965293e64134fdb498e23
ktaylora <kyle.a.taylor@gmail.com>
2018-03-08 12:43:56 -0700

    house keeping

--- ee_scripts/ee_generate_surface_wetness_assets.py
+++ ee_scripts/ee_generate_surface_wetness_assets.py
@@ -1,8 +1,11 @@
-""" This script pulls from the Landsat 8 satellite to generate an estimate of recent surface wetness """
+#!/usr/bin/env python2
 
-import config
+""" This script pulls from the Landsat 8 satellite to generate an estimate of
+recent surface wetness """
+
 import ee
-
+import json
+import time
 
 def now_minus_n_months(*args):
     """ accept a single positional argument (month) specifying how far from now
@@ -37,8 +40,13 @@ def simple_water_algorithm(image):
 def get_fc_coordinates(collection=None):
     return collection.geometry().getInfo()['coordinates']
 
+def setAssetGloballyReadable(assetId="users/kyletaylor/shared/LC8dynamicwater"):
+   acl = ee.data.getAssetAcl(assetId)
+   acl['all_users_can_read'] = True
+   acl.pop('owners')
+   ee.data.setAssetAcl(assetId, json.dumps(acl))
 
-def exportImageToAsset(image, assetId, region):
+def exportImageToAsset(image=None, assetId=None, region=None, timeout_minutes=30):
     task = ee.batch.Export.image.toAsset(
         image=image,
         assetId=assetId,
@@ -47,8 +55,30 @@ def exportImageToAsset(image, assetId, region):
         region=region,
         description='dynamic water over the last 10 months'
     )
-    # start the task and then return status object to the user
+    # delete the asset if it exists
+    try:
+      ee.batch.data.deleteAsset(assetId)
+    except ee.ee_exception.EEException:
+      pass
+    # start the task and monitor our progress
     task.start()
+    while str(task.status()['state']) != 'COMPLETED':
+      
+      time.sleep(5)
+      
+      try:
+        task_runtime = (int(task.status()['update_timestamp_ms']) - int(task.status()['start_timestamp_ms'])) / 1000
+      except Exception as e:
+        return(task)
+      # if we take longer than 30 minutes, throw an error
+      if task_runtime > (60 * timeout_minutes):
+        raise ee_exception.EEException('EE task timed out')
+      if task.status()['state'] == 'FAILED':
+        raise ee_exception.EEException('EE task FAILED')
+    # if we succeeded, let's set the asset to globally readable
+    if task.status():
+       setAssetGloballyReadable(assetId)
+    # return the task to the user for inspection
     return (task)
 
 
@@ -77,8 +107,6 @@ if __name__ == "__main__":
     # define our time-series information
     last_wet_scene = simple_water_algorithm(image_from_ls8_collection(hist_date=now_minus_n_months(10)))
     last_wet_scene = last_wet_scene.updateMask(last_wet_scene.neq(0))
-
-    ee.batch.data.deleteAsset('users/kyletaylor/shared/LC8dynamicwater')
 
     # export the resulting "water
     status = exportImageToAsset(

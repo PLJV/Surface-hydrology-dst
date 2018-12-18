8eede85693a866a9bc8f8b0d88c0b0a1849f5933
Kyle Taylor <kyle.a.taylor@gmail.com>
2018-12-18 11:45:02 -0500

    house keeping

--- ee_scripts/ee_generate_surface_wetness_assets.py
+++ ee_scripts/ee_generate_surface_wetness_assets.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python2
 
-""" This script pulls from the Landsat 8 satellite to generate an estimate of
-recent surface wetness """
+__author__ = "Kyle Taylor, Alex Daniels"
+__copyright__ = "Copyright 2018, Playa Lakes Joint Venture"
+__credits__ = ["Alex Daniels", "Kyle Taylor"]
+__license__ = "GPL"
+__version__ = "3"
+__maintainer__ = "Kyle Taylor"
+__email__ = "kyle.taylor@pljv.org"
+__status__ = "Production"
+__description__ = "This script pulls from the Landsat 8 satellite to generate an estimate of recent surface wetness"
 
 import ee
 import json
@@ -10,8 +17,10 @@ import time
 _SLEEP_TIME_SECONDS = 60 # Usually takes about 9 minutes to generate this asset
 
 def now_minus_n_months(*args):
-    """ accept a single positional argument (month) specifying how far from now
-    to go back in our landsat imagery"""
+    """
+    accept a single positional argument (month) specifying how far from now
+    to go back in our landsat imagery
+    """
     now = ee.datetime.datetime.now()
     dst_year = now.year
     dst_month = now.month - args[0]
@@ -22,9 +31,11 @@ def now_minus_n_months(*args):
     return str(dst_year) + '-' + str(dst_month) + '-' + str(dst_day)
 
 def qa(pimage):
-  """ a function to mask out unwanted pixels using 5471 as a bitwise mask
+  """
+  a function to mask out unwanted pixels using 5471 as a bitwise mask
   because it's binary equivalent is the inverse of what we want in the qa
-  band info gleaned from https://landsat.usgs.gov/collectionqualityband """
+  band info gleaned from https://landsat.usgs.gov/collectionqualityband 
+  """
   image = pimage.select('BQA').bitwiseAnd(5471).neq(0).eq(0)
   return pimage.updateMask(image);
 
@@ -33,11 +44,13 @@ def addQualityBands(image):
    return image.addBands(image.metadata('system:time_start'))
 
 def image_from_ls8_collection(collection_id='LANDSAT/LC08/C01/T1_RT', cloud_mask=5):
-    """The image collection and sorting that all out. Get the most recent image from a given collection"""
+    """
+    The image collection and sorting that all out. Get the most recent image from a given collection
+    """
     collection = ee.ImageCollection(collection_id). \
         filterMetadata('CLOUD_COVER', 'less_than', cloud_mask). \
         filterMetadata('CLOUD_COVER', 'greater_than', -0.1). \
-        filterBounds(ee.FeatureCollection('ft:1fRY18cjsHzDgGiJiS2nnpUU3v9JPDc2HNaR7Xk8').filter(ee.Filter.eq('Name','Kansas')))
+        filterBounds(ee.FeatureCollection('users/adaniels/tl_2014_us_state').filter(ee.Filter.eq('NAME', 'Kansas')));
     # map the qa function over the collection to clean out snow, shadows, clouds
     collection_qa = collection.map(qa)
     # add a temporal band to the collection to sort by later
@@ -45,12 +58,11 @@ def image_from_ls8_collection(collection_id='LANDSAT/LC08/C01/T1_RT', cloud_mask
     # build a mosiac from the resulting collection using the most recent pixel
     return collection_qa_withtime.qualityMosaic('system:time_start')
 
-def get_datetime_qa_band(image):
-    return image.select('system:time_start')
-
 def water_ruiz2014(image):
-    """ surface water detection from Ruiz et al. 2014 that just uses band 6
-    and band 4 """
+    """
+    surface water detection from Ruiz et al. 2014 that just uses band 6
+    and band 4
+    """
     band_magic = {
         'B4': image.select('B4'),
         'B6': image.select('B6'),
@@ -59,7 +71,9 @@ def water_ruiz2014(image):
     return band_magic['blank'].where(band_magic['B6'].lte(band_magic['B4']), 1)
 
 def water_mcfeeters1996(plandsatimage):
-  """the normalized difference water index method (green - nir)/(green + nir)"""
+  """
+  The normalized difference water index method (green - nir)/(green + nir)
+  """
   b3 = plandsatimage.select('B3');
   b5 = plandsatimage.select('B5');
   ndwi = b3.subtract(b5).divide((b3.add(b5)));
@@ -68,8 +82,10 @@ def water_mcfeeters1996(plandsatimage):
   return out.updateMask(out);
 
 def water_xu2007(plandsatimage):
-  """ the modified normalized difference water index method
-  (green - swir)/(green + swir)"""
+  """ 
+  The modified normalized difference water index method
+  (green - swir)/(green + swir)
+  """
   b3 = plandsatimage.select('B3');
   b6 = plandsatimage.select('B6');
   mndwi = b3.subtract(b6).divide((b3.add(b6)));
@@ -78,7 +94,7 @@ def water_xu2007(plandsatimage):
   return out.updateMask(out);
 
 def get_fc_coordinates(collection=None):
-    return collection.geometry().getInfo()['coordinates']
+    return collection.geometry().getInfo()
 
 def set_asset_globally_readable(assetId=None):
    acl = ee.data.getAssetAcl(assetId)
@@ -86,7 +102,7 @@ def set_asset_globally_readable(assetId=None):
    acl.pop('owners')
    ee.data.setAssetAcl(assetId, json.dumps(acl))
 
-def exportImageToAsset(image=None, assetId=None, region=None, timeout_minutes=30):
+def exportImageToAsset(image=None, assetId=None, region=None, timeout_minutes=90):
     task = ee.batch.Export.image.toAsset(
         image=image,
         assetId=assetId,
@@ -141,30 +157,17 @@ if __name__ == "__main__":
     ee.Initialize()
 
     # import the geometry for Kansas
-    kansas = ee.FeatureCollection('ft:1fRY18cjsHzDgGiJiS2nnpUU3v9JPDc2HNaR7Xk8').filter(ee.Filter.eq('Name','Kansas'))
+    kansas = ee.FeatureCollection('users/adaniels/tl_2014_us_state').filter(ee.Filter.eq('NAME', 'Kansas'));
 
     # define our time-series information
-    last_wet_scene = water_ruiz2014(
-        image_from_ls8_collection('LANDSAT/LC08/C01/T1_RT')
-    )
+    last_wet_scene = water_ruiz2014((image_from_ls8_collection('LANDSAT/LC08/C01/T1_RT')))
 
     # mask out pixels that have never been wet over a 30 year period
-    last_wet_scene = last_wet_scene.multiply(
-        ee.Image("users/kyletaylor/shared/long_run_surface_wetness_mask")
-    )
-    datetime = get_datetime_qa_band(
-        image_from_ls8_collection('LANDSAT/LC08/C01/T1_RT')
-    )
-    # export the resulting surface wetness band
+    last_wet_scene = last_wet_scene.multiply(ee.Image("users/kyletaylor/shared/long_run_surface_wetness_mask"))
+
+    # export the resulting "water
     status = exportImageToAsset(
         last_wet_scene,
         'users/kyletaylor/shared/LC8dynamicwater',
-        region=get_fc_coordinates(kansas)
-    )
-
-    # add add the
-    status = exportImageToAsset(
-        datetime,
-        'users/kyletaylor/shared/LC8dynamicwater_datetime',
-        region=get_fc_coordinates(kansas)
+        region=kansas.geometry().bounds().coordinates().getInfo()
     )

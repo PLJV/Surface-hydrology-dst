fea531da822eb62439662f0c8b3423ab119227db
Kyle Taylor <kyle.a.taylor@gmail.com>
2019-02-09 17:54:32 -0700

    house keeping -- validated working

--- ee_scripts/ee_generate_surface_wetness_assets.py
+++ ee_scripts/ee_generate_surface_wetness_assets.py
@@ -15,6 +15,9 @@ import json
 import time
 
 _SLEEP_TIME_SECONDS = 60 # Usually takes about 9 minutes to generate this asset
+_SCALE = 30 # units here are meters
+_MAX_PIXELS = 3E09 # Max pixels for an export to use before throwing an error
+_MAX_CLOUD_COVER = 10 # Maximum cloud cover contamination to accept in landsat
 
 def now_minus_n_months(*args):
     """
@@ -30,37 +33,45 @@ def now_minus_n_months(*args):
         dst_month = dst_month + 12
     return str(dst_year) + '-' + str(dst_month) + '-' + str(dst_day)
 
-def qa(pimage):
+def add_qa_band(image):
   """
   a function to mask out unwanted pixels using 5471 as a bitwise mask
   because it's binary equivalent is the inverse of what we want in the qa
   band info gleaned from https://landsat.usgs.gov/collectionqualityband 
   """
-  image = pimage.select('BQA').bitwiseAnd(5471).neq(0).eq(0)
-  return pimage.updateMask(image);
+  qa_mask= image\
+    .select('BQA')\
+    .bitwiseAnd(5471)\
+    .neq(0)\
+    .eq(0)
 
-def addQualityBands(image):
+  return image\
+    .updateMask(qa_mask)
+
+def add_time_stamp_band(image):
    """ a function to add the time stamp bands """
-   return image.addBands(image.metadata('system:time_start'))
+   return ee.Image(image)\
+     .addBands(ee.Image.constant(image.get(u'system:time_start'))\
+     .rename("time")\
+     .cast({"time": "long"})\
+     .copyProperties(image, ['system:time_start']))
 
-def image_from_ls8_collection(collection_id='LANDSAT/LC08/C01/T1_RT', cloud_mask=5):
+def image_mosaic_from_ls8_collection(collection_id='LANDSAT/LC08/C01/T1_RT', cloud_mask=_MAX_CLOUD_COVER, region=None):
     """
     The image collection and sorting that all out. Get the most recent image from a given collection
     """
-    collection = ee.ImageCollection(collection_id). \
-        filterMetadata('CLOUD_COVER', 'less_than', cloud_mask). \
-        filterMetadata('CLOUD_COVER', 'greater_than', -0.1). \
-        filterBounds(ee.FeatureCollection('users/adaniels/tl_2014_us_state').filter(ee.Filter.eq('NAME', 'Kansas')));
+    collection = ee.ImageCollection(collection_id).\
+        filterMetadata('CLOUD_COVER', 'less_than', cloud_mask).\
+        filterMetadata('CLOUD_COVER', 'greater_than', -0.1).\
+        filterBounds(region)
     
-    # add a temporal band to the collection to sort by later
-    collection_withtime = collection.map(addQualityBands)
-    
-    # map the qa function over the collection to clean out snow, shadows, clouds
-    collection_qa_withtime = collection_withtime.map(qa)
+    # add a time band
+    collection = collection.map(add_time_stamp_band)
+    # map the qa function over the collection to mask out snow, shadows, clouds
+    collection = collection.map(add_qa_band)
 
-    
     # build a mosiac from the resulting collection using the most recent pixel
-    return collection_qa_withtime.qualityMosaic('system:time_start')
+    return collection.qualityMosaic('time')
 
 def water_ruiz2014(image):
     """
@@ -78,24 +89,24 @@ def water_mcfeeters1996(plandsatimage):
   """
   The normalized difference water index method (green - nir)/(green + nir)
   """
-  b3 = plandsatimage.select('B3');
-  b5 = plandsatimage.select('B5');
-  ndwi = b3.subtract(b5).divide((b3.add(b5)));
-  blank = ee.Image(0);
-  out = blank.where(ndwi.gte(0),1);
-  return out.updateMask(out);
+  b3 = plandsatimage.select('B3')
+  b5 = plandsatimage.select('B5')
+  ndwi = b3.subtract(b5).divide((b3.add(b5)))
+  blank = ee.Image(0)
+  out = blank.where(ndwi.gte(0),1)
+  return out.updateMask(out)
 
 def water_xu2007(plandsatimage):
   """ 
   The modified normalized difference water index method
   (green - swir)/(green + swir)
   """
-  b3 = plandsatimage.select('B3');
-  b6 = plandsatimage.select('B6');
-  mndwi = b3.subtract(b6).divide((b3.add(b6)));
-  blank = ee.Image(0);
-  out = blank.where(mndwi.gte(0),1);
-  return out.updateMask(out);
+  b3 = plandsatimage.select('B3')
+  b6 = plandsatimage.select('B6')
+  mndwi = b3.subtract(b6).divide((b3.add(b6)))
+  blank = ee.Image(0)
+  out = blank.where(mndwi.gte(0),1)
+  return out.updateMask(out)
 
 def get_fc_coordinates(collection=None):
     return collection.geometry().getInfo()
@@ -105,18 +116,19 @@ def set_asset_globally_readable(assetId=None):
    acl['all_users_can_read'] = True
    acl.pop('owners')
    ee.data.setAssetAcl(assetId, json.dumps(acl))
-   test = ee.data.getAssetAcl(assetId)
-   if not test['all_users_can_read']:
+   validate = ee.data.getAssetAcl(assetId)
+   if not validate['all_users_can_read']:
         time.sleep(5)
         set_asset_globally_readable(assetId=assetId)
         
 
-def exportImageToAsset(image=None, assetId=None, region=None, timeout_minutes=90, description='Generating LS8 last wet scene product'):
+def export_as_asset(image=None, assetId=None, region=None, timeout_minutes=90, description='Generating LS8 last wet scene product', scale=_SCALE):
     task = ee.batch.Export.image.toAsset(
         image=image,
         assetId=assetId,
-        scale=10,
-        maxPixels=40000000000,
+        scale=scale,
+        description=description,
+        maxPixels=_MAX_PIXELS,
         region=region
     )
     # delete the asset if it exists
@@ -148,15 +160,19 @@ def exportImageToAsset(image=None, assetId=None, region=None, timeout_minutes=90
     return (task)
 
 
-def exportImageToDrive(image, assetId):
+def export_image_to_drive(image, assetId, description='Generating LS8 last wet scene product', scale=_SCALE):
+    # define our EE task options
     task_config = {
-        'description': 'Generating LS8 last wet scene product',
-        'scale': 30,
-        'maxPixels': 400000000
+        'description': description,
+        'scale': scale,
+        'maxPixels': _MAX_PIXELS
     }
-    task = ee.batch.Export.image(image, assetId, task_config)
-    task.start()
-    return (task)
+    task = ee.batch.Export.image(
+        image, 
+        assetId, 
+        task_config
+      ).start()
+    return(task)
 
 
 if __name__ == "__main__":
@@ -165,28 +181,35 @@ if __name__ == "__main__":
     ee.Initialize()
 
     # import the geometry for Kansas
-    kansas = ee.FeatureCollection('users/adaniels/tl_2014_us_state').filter(ee.Filter.eq('NAME', 'Kansas'));
-
-    # define our time-series information
-    last_wet_scene = water_ruiz2014((image_from_ls8_collection('LANDSAT/LC08/C01/T1_RT')))
-
-    # mask out pixels that have never been wet over a 30 year period
-    last_wet_scene = last_wet_scene.multiply(ee.Image("users/kyletaylor/shared/long_run_surface_wetness_mask"))
+    kansas = ee.FeatureCollection('users/adaniels/tl_2014_us_state').filter(ee.Filter.eq('NAME', 'Kansas'))
+    region_boundary = kansas.geometry().bounds().coordinates().getInfo()
     
-    #create time map
-    time_image = image_from_ls8_collection('LANDSAT/LC08/C01/T1_RT').select('system:time_start')
+    # generate an ee.Image of (unix) time since scene acquisition 
+    unix_time_image = ee.ImageCollection('LANDSAT/LC08/C01/T1_RT')\
+      .filterMetadata('CLOUD_COVER', 'less_than', _MAX_CLOUD_COVER)\
+      .filterMetadata('CLOUD_COVER','greater_than',-0.1)\
+      .filterBounds(kansas.geometry().bounds())\
+      .map(add_time_stamp_band)\
+      .qualityMosaic('time')\
+      .select('time')
+
+    # define our time-series information and mask out pixels that have never been wet over a 30 year period
+    last_wet_scene = water_ruiz2014(
+        image_mosaic_from_ls8_collection('LANDSAT/LC08/C01/T1_RT', region=kansas)
+      ).multiply(ee.Image("users/kyletaylor/shared/long_run_surface_wetness_mask"))
 
     # export the resulting "water
-    status = exportImageToAsset(
-        last_wet_scene,
-        'users/kyletaylor/shared/LC8dynamicwater',
-        region=kansas.geometry().bounds().coordinates().getInfo(),
-        description='Generating LS8 last wet scene product'
-    )
+    status = export_as_asset(
+        image=last_wet_scene,
+        assetId='users/kyletaylor/shared/LC8dynamicwater',
+        region=region_boundary,
+        description='Generating LS8 last wet scene product',
+        scale=10
+      )
     
-    status = exportImageToAsset(
-        time_image,
-        'users/kyletaylor/shared/time_of_landsat_mosaic_pixel',
-        region=kansas.geometry().bounds().coordinates().getInfo(),
-        description='Generating landsat time product'
-    )
+    status = export_as_asset(
+        image=unix_time_image,
+        assetId='users/kyletaylor/shared/time_of_landsat_mosaic_pixel',
+        region=region_boundary,
+        description='Generating LS8 unix time product'
+      )
